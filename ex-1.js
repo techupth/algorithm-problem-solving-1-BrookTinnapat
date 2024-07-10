e93b4aed25ea50a822360a91f6d632b79c3cb1e4
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-07-10 14:26:47 +0700

    submit

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < numbers.length; i++) {
+    let sum = numbers[i] + numbers[i - 1];
+    if (sum === target) {
+      return `${true} by ${numbers[i]} + ${numbers[i - 1]} = ${sum}`;
+    }
+  }
+}
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const target2 = 23;
+console.log(twoSum(nums, target));
+console.log(twoSum(nums, target2));
